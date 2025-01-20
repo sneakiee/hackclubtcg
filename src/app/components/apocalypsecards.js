74b93587688de2b9a7d95884eba8cf97cff53aa4
sneakiee <denfun.funden@gmail.com>
2025-01-19 21:41:44 -0500

    initial

---
+++ src/app/components/apocalypsecards.js
@@ -0,0 +1,24 @@
+import hackyHacker from "../art/cards/apocalypse/1_hackyhacker.png";
+import infectedOrpheus from "../art/cards/apocalypse/2_infectedorpheus.png";
+import survivorOrpheus from "../art/cards/apocalypse/3_survivororpheus.png";
+import watcherOrpheus from "../art/cards/apocalypse/4_watcherorpheus.png";
+import skullpup from "../art/cards/apocalypse/5_skullpup.png";
+import bats from "../art/cards/apocalypse/6_bats.png";
+import trashPanda from "../art/cards/apocalypse/7_trashpanda.png";
+import hackoon from "../art/cards/apocalypse/8_hackoon.png";
+import angryGoose from "../art/cards/apocalypse/9_angrygoose.png";
+import dragon from "../art/cards/apocalypse/10_dragon.png";
+import opossum from "../art/cards/apocalypse/11_opossum.png";
+import trojanVirus from "../art/cards/apocalypse/12_trojanvirus.png";
+import murderOfCrows from "../art/cards/apocalypse/13_murderofcrows.png";
+import otter from "../art/cards/apocalypse/14_otter.png";
+import cathorde from "../art/cards/apocalypse/15_cathorde.png";
+import rathorde from "../art/cards/apocalypse/16_rathorde.png";
+import cat from "../art/cards/apocalypse/17_cat.png";
+import rat from "../art/cards/apocalypse/18_rat.png";
+
+export default function ApocalypseCards() {
+    <div className="flex flex-wrap justify-center xl:justify-between max-w-8x1 gap-8 py-10 w=full">
+        <img className="rounded-lg h-auto max-w-full" src={hackyHacker} alt="Hacky Hacker"/>
+    </div>
+}
