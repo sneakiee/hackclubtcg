9522aa8b40293b85fd1e3a5edd610890fcae1859
mikael <101999696+sneakiee@users.noreply.github.com>
2025-01-20 19:36:03 -0500

    add cards components

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import ApocalypseCards from "./components/apocalypsecards";
+import CounterspellCards from "./components/counterspellcards"
 
 export default function Home() {
   return (
     <main>
       <ApocalypseCards/>
+      <CounterspellCards/>
     </main>
   );
 }
